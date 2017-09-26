9b52580771b0a9b4645cfc9b6da2b297ba609f08
vamsitallapudi <mail4vamsikrishna@gmail.com>
2017-09-26 22:50:52 +0530

    Initial Commit

---
+++ app/src/main/java/com/coderefer/multilanguagesupporteg/MainActivity.java
@@ -0,0 +1,166 @@
+package com.coderefer.multilanguagesupporteg;
+
+import android.content.SharedPreferences;
+import android.content.res.Configuration;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.content.ContextCompat;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
+
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    private static final String LOCALE_KEY = "localekey";
+    private static final String HINDI_LOCALE = "hi";
+    private static final String ENGLISH_LOCALE = "en_US";
+    private static final String LOCALE_PREF_KEY = "localePref";
+    private ImageView ivWelcome;
+    private Locale locale;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.setContentView(R.layout.activity_main);
+
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        setTitle(R.string.app_name);
+        SharedPreferences sp = getSharedPreferences(LOCALE_PREF_KEY, MODE_PRIVATE);
+        String localeString = sp.getString(LOCALE_KEY, ENGLISH_LOCALE);
+        setupImageBasedOnLocale(localeString);
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    private void setupImageBasedOnLocale(String localeString) {
+        ivWelcome = (ImageView) findViewById(R.id.iv_welcome);
+        if(localeString.equals(HINDI_LOCALE)){
+            ivWelcome.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.swagath));
+        } else {
+            ivWelcome.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.welcome));
+
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            Resources resources = getResources();
+            SharedPreferences sharedPreferences = getSharedPreferences("localePref", MODE_PRIVATE);
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+
+            if(sharedPreferences.getString(LOCALE_KEY, ENGLISH_LOCALE).equals(HINDI_LOCALE)){
+                locale = new Locale(ENGLISH_LOCALE);
+                editor.putString(LOCALE_KEY, ENGLISH_LOCALE);
+            } else {
+                locale = new Locale(HINDI_LOCALE);
+                editor.putString(LOCALE_KEY, HINDI_LOCALE);
+            }
+            editor.apply();
+
+            Configuration configuration = resources.getConfiguration();
+            configuration.setLocale(locale);
+            getBaseContext().getResources().updateConfiguration(configuration,
+                    getBaseContext().getResources().getDisplayMetrics());
+            recreate();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+
+        SharedPreferences sharedPreferences = getSharedPreferences("localePref", MODE_PRIVATE);
+
+        MenuItem item = menu.getItem(0);
+        if(sharedPreferences.getString(LOCALE_KEY, "en_US").equals(HINDI_LOCALE)){
+            item.setTitle("English");
+        } else {
+            item.setTitle("Hindi");
+
+        }
+
+        return true;
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
